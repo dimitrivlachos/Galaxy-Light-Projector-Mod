8a40a90fa6864ea6ca7fc1580389f726fcc7e975
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-08-15 23:27:44 +0100

    Created basic blinker

--- GalaxyCode/src/main.cpp
+++ GalaxyCode/src/main.cpp
@@ -2,17 +2,35 @@
 
 // put function declarations here:
 int myFunction(int, int);
+void toggleLED();
+
+#define ONBOARD_LED 2
+bool ledOn = false;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("Hello World!");
+
+  pinMode(ONBOARD_LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  toggleLED();
+  delay(1000);
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
+}
+
+void toggleLED() {
+  if(ledOn) {
+    digitalWrite(ONBOARD_LED, LOW);
+    ledOn = false;
+  } else {
+    digitalWrite(ONBOARD_LED, HIGH);
+    ledOn = true;
+  }
 }
