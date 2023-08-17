d9088861e6e3d270e7cc3b8e7146f89d730c916f
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-08-17 23:43:42 +0100

    Improved syntax

--- GalaxyCode/src/main.cpp
+++ GalaxyCode/src/main.cpp
@@ -100,25 +100,25 @@ void setup() {
 
   Serial.print("Initialising Task1... ");
   xTaskCreatePinnedToCore(
-             Task1code, /* Task function. */
-             "Task1",   /* name of task. */
-             10000,     /* Stack size of task */
-             NULL,      /* parameter of the task */
-             1,         /* priority of the task */
-             &Task1,    /* Task handle to keep track of created task */
-             0);        /* pin task to core 0 */          
+    Task1code, /* Task function. */
+    "Task1",   /* name of task. */
+    10000,     /* Stack size of task */
+    NULL,      /* parameter of the task */
+    1,         /* priority of the task */
+    &Task1,    /* Task handle to keep track of created task */
+    0);        /* pin task to core 0 */          
   delay(500); 
 
   Serial.print("Initialising Task2... ");
   xTaskCreatePinnedToCore(
-                    Task2code,   /* Task function. */
-                    "Task2",     /* name of task. */
-                    10000,       /* Stack size of task */
-                    NULL,        /* parameter of the task */
-                    1,           /* priority of the task */
-                    &Task2,      /* Task handle to keep track of created task */
-                    1);          /* pin task to core 1 */
-    delay(500); 
+    Task2code,   /* Task function. */
+    "Task2",     /* name of task. */
+    10000,       /* Stack size of task */
+    NULL,        /* parameter of the task */
+    1,           /* priority of the task */
+    &Task2,      /* Task handle to keep track of created task */
+    1);          /* pin task to core 1 */
+  delay(500); 
 }
 
 void loop() {
